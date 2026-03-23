b24831ecf3069df8890ed94ee8bee85d13db59e8
almostcomin <alfonsocomin@gmail.com>
2026-03-23 21:24:29 +0100

    Fix 3rdParty build script to handle build dependencies

--- 3rdParty/BuildAll.py
+++ 3rdParty/BuildAll.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 build_dir = os.path.join(os.path.dirname(__file__), "Build")
-
-scripts = [
-    f for f in os.listdir(build_dir)
-    if f.endswith(".py")
-]
+scripts = {"Build_DirectX-Headers.py", "Build_DirectXTex.py", "Build_SDL.py", "Build_imgui.py", "Build_stb.py"}
 
 for script in scripts:
     path = os.path.join(build_dir, script)
